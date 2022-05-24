a216e85dc840c45373cb21c818b610030498fe59
Joy <parkerjn90@gmail.com>
2022-05-24 17:15:13 -0400

    add date formatting dependency

--- src/components/Questions/QuestionListEntry.jsx
+++ src/components/Questions/QuestionListEntry.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+// import differenceInCalendarISOYears from 'date-fns/difference_in_calendar_iso_years'
 
 const QuestionListEntry = ({question}) => {
-  console.log('props in list entry');
-  console.log(question);
 
   return(
     <>
-    <h4 className="question_title">Q: Test Question?</h4><p className="question_helpful">Helpful? Yes&#40;#&#41;</p>
-
+    <h4 className="question_title">{`Q: ${question.question_body}`}</h4><p className="question_helpful">Helpful? Yes&#40;#&#41;</p>
+    <h5>{`asked by ${question.asker_name} ${question.question_date}`}</h5>
     </>
   )
 

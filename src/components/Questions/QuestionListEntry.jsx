55d6aefdf7967cf14cda44fef0f8dcbb59053e00
Joy <parkerjn90@gmail.com>
2022-05-24 16:04:21 -0400

    destructure props

--- src/components/Questions/QuestionListEntry.jsx
+++ src/components/Questions/QuestionListEntry.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const QuestionListEntry = (props) => {
+const QuestionListEntry = ({question}) => {
   console.log('props in list entry');
-  console.log(props);
+  console.log(question);
 
   return(
-    <h4>Q: Test Question?</h4><p>Helpful? Yes&#40;#&#41;</p>
+    <>
+    <h4 className="question_title">Q: Test Question?</h4><p className="question_helpful">Helpful? Yes&#40;#&#41;</p>
 
+    </>
   )
 
 }

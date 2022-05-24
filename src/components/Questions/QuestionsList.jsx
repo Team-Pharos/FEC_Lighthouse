d9562943b84827c7d66834ee6060c6cd87c059a3
Joy <parkerjn90@gmail.com>
2022-05-24 15:35:22 -0400

    destructure props so that only 'questions' is required as variable when referring to questions array

--- src/components/Questions/QuestionsList.jsx
+++ src/components/Questions/QuestionsList.jsx
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import QuestionListEntry from './QuestionListEntry.jsx';
 
-const QuestionsList =(props) => {
-
-  console.log(props);
+const QuestionsList = ({questions}) => {
 
   return(
     <>
-    {props.questions.map((question) => {
+    {questions.map((question) => {
       <QuestionListEntry key={question.question_id} question={question}/>
     })}
     </>

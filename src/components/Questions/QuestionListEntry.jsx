d815db5bdf2cae5435d855372594d39bca9206da
Joy <parkerjn90@gmail.com>
2022-05-24 15:31:27 -0400

    add QuestionListEntry functional component skeleton code

---
+++ src/components/Questions/QuestionListEntry.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const QuestionListEntry = (props) => {
+
+  return(
+    <>
+    </>
+  )
+
+}
+
+export default QuestionListEntry;

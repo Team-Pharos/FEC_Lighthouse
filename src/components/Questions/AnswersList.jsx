be715ff944333c37a9b53bd319c39e8c50b7da81
Joy <parkerjn90@gmail.com>
2022-05-25 12:27:43 -0400

    update documentation to accommodate new Answer List component

--- src/components/Questions/AnswersList.jsx
+++ src/components/Questions/AnswersList.jsx
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const AnswersList = (props) => {
+const AnswersList = ({answers}) => {
   return (
     <>
+    <h4 className="answer_title">A: Answer Title</h4><p className="answer helpful">Helpful? Yes&#40;#&#41; Report</p>
+    <h5>by: username on date</h5>
     </>
   )
 }

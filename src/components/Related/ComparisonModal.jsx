f51b66fcce59650582c611fe07854c7c821cf5f4
Jordan Addleman <xibalbarising@gmail.com>
2022-05-25 03:46:05 -0400

    WIP, implementing useEffect hook in Related, getting atelier API data into Related front end

--- src/components/Related/ComparisonModal.jsx
+++ src/components/Related/ComparisonModal.jsx
@@ -1,20 +1,24 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect} from 'react';
 
-const ComparisonModal = () => {
+const ComparisonModal = ({productId, productDetails}) => {
   return (
     <div>
       <h1>Comparison Modal</h1>
       <table>
-        <tr>
-          <th>Current Product</th>
-          <th>Characteristic</th>
-          <th>Compared Product</th>
-        </tr>
-        <tr>
-          <td>Value</td>
-          <td>Value</td>
-          <td>Value</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Current Product</th>
+            <th>Characteristic</th>
+            <th>Compared Product</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{productId}</td>
+            <td>Value</td>
+            <td>Value</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );

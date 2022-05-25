7ecb1ddce3beecddc76ae05ae508e8f004cf2d4d
Jordan Addleman <xibalbarising@gmail.com>
2022-05-25 01:59:25 -0400

    added a table, icons and buttons to Related and Outfit components

--- src/components/Related/ComparisonModal.jsx
+++ src/components/Related/ComparisonModal.jsx
@@ -4,6 +4,18 @@ const ComparisonModal = () => {
   return (
     <div>
       <h1>Comparison Modal</h1>
+      <table>
+        <tr>
+          <th>Current Product</th>
+          <th>Characteristic</th>
+          <th>Compared Product</th>
+        </tr>
+        <tr>
+          <td>Value</td>
+          <td>Value</td>
+          <td>Value</td>
+        </tr>
+      </table>
     </div>
   );
 }

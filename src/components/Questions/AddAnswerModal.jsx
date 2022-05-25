9581abf8de9a8407422d6c9d29a64e31cd8f5eef
Joy <parkerjn90@gmail.com>
2022-05-25 16:44:34 -0400

    fixed Input typo ==> styled.input

--- src/components/Questions/AddAnswerModal.jsx
+++ src/components/Questions/AddAnswerModal.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
-const Input = styled.Input`
+const Input = styled.input`
   width: 200px;
 `;
 

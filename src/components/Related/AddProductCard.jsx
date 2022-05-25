859d090a21cc8fc76092608bd5e2680ba73287a1
Jordan Addleman <xibalbarising@gmail.com>
2022-05-24 20:33:14 -0400

    added RTL test to be used with Jest for a snapshot test. Need to sort out more dependencies, RTL??? RTR???

--- src/components/Related/AddProductCard.jsx
+++ src/components/Related/AddProductCard.jsx
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from "react";
 const AddProductCard = () => {
   return (
     <div>
-      <h1>Add Product Card</h1>
+      <h1 onClick={window.alert('😤Just you wait until I code up a real nice modal🤩')}>Add Product Card</h1>
     </div>
   );
 }
--- src/components/Related/ProductCardRI.jsx
+++ src/components/Related/ProductCardRI.jsx
@@ -4,6 +4,7 @@ const ProductCardRI = () => {
   return (
     <div>
       <h1>Product Card Related Items</h1>
+      <button onClick={window.alert('Show Comparison Modal')}>Action Button 'star'</button>
     </div>
   );
 }

3dac2679d37e676f48f41d9be7c520935474aa7b
Joy <parkerjn90@gmail.com>
2022-05-27 12:13:41 -0400

    add event handler to handle submit

--- src/components/Questions/AddAnswerModal.jsx
+++ src/components/Questions/AddAnswerModal.jsx
@@ -3,11 +3,34 @@ import axios from 'axios';
 import styled from 'styled-components';
 import { Input, OuterModal, InnerModal, BodyInput, Modal } from './Styles.jsx';
 
-const AddAnswer = ({ productName, questionBody, addAnswer, onClose }) => {
+const AddAnswer = ({ productName, question_id, questionBody, addAnswer, onClose }) => {
 
-  const [bodyInput, setBodyInput] = useState('');
-  const [nicknameInput, setNicknameInput] = useState('');
-  const [emailInput, setEmailInput] = useState('');
+  const [bodyInput, setBodyInput] = useState(null);
+  const [nicknameInput, setNicknameInput] = useState(null);
+  const [emailInput, setEmailInput] = useState(null);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios({
+      method: 'post',
+      url: '/postQuestion',
+      data: {
+        body: bodyInput,
+        name: nicknameInput,
+        email: emailInput,
+        product_id: question_id
+      }
+    })
+      // .then(() => {
+      //   //this is where you'll add it to state to the top of the list
+      // })
+      .then(() => {
+        onClose();
+      })
+      .catch((err) => {
+        console.log(`error: ${err}`);
+      })
+  }
 
 
   if (addAnswer === false) {
@@ -22,14 +45,14 @@ const AddAnswer = ({ productName, questionBody, addAnswer, onClose }) => {
           <h5>{`${productName}: ${questionBody}`}</h5>
           <form>
           <p>Your Answer *</p>
-          <BodyInput type="text" required maxlength="1000"/>
+          <BodyInput type="text" required maxlength="1000" onChange={(e) => {setBodyInput(e.target.value);}}/>
           <p>What is your nickname? *</p>
-          <Input type="text" required maxlength="60" placeholder="Example: jack543!" />
+          <Input type="text" required maxlength="60" placeholder="Example: jack543!" onChange={(e) => {setNicknameInput(e.target.value)}}/>
           <h6>For privacy reasons, do not use your full name or email address</h6>
           <p>Email *</p>
-          <Input type="email" required maxlength="60" placeholder="Example: jack@email.com" />
+          <Input type="email" required maxlength="60" placeholder="Example: jack@email.com" onChange={(e) => {setEmailInput(e.target.value)}}/>
           <h6>For authentication reasons, you will not be emailed</h6>
-          <input type="submit" value="SubmitAnswer"></input><button onClick={onClose}>Close</button>
+          <button onClick={submitHandler}>SubmitAnswer</button><button onClick={onClose}>Close</button>
           </form>
       </Modal>
         </InnerModal>

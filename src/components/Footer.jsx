b3cbf731ac7470fa8e317d2156d61657d472dd44
Joy <parkerjn90@gmail.com>
2022-06-02 12:28:31 -0400

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { CompanyHeading, InnerDiv, CompanyTitle, CompanyLogo } from '../../styles/SiteStyles.jsx';
+import {FaShoppingCart} from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+  <CompanyHeading></CompanyHeading>
+  )
+}
+
+export default Footer;

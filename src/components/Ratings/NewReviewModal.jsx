1ce8fc5332ffc12b8177584b3361a0adaff6530a
kkranzfelder <kkranzfelder@gmail.com>
2022-06-02 11:31:42 -0400

    Trying to fix radio button response.

--- src/components/Ratings/NewReviewModal.jsx
+++ src/components/Ratings/NewReviewModal.jsx
@@ -35,6 +35,7 @@ const NewReviewModal = ({ closeModal, metaData, productId, productName }) => {
   let [reviewBody, setReviewBody] = useState('');
   let [summary, setSummary] = useState('');
   let [recommend, setRecommend] = useState();
+  let [sizeSelection, setSize] = useState();
   let [fitSelection, setFit] = useState();
   let [comfortSelection, setComfort] = useState();
   let [widthSelection, setWidth] = useState();
@@ -83,6 +84,32 @@ const NewReviewModal = ({ closeModal, metaData, productId, productName }) => {
   let charHandler = (event) => {
     event.preventDefault();
     let name = event.target.name;
+    let id = event.target.id;
+    console.log(id);
+    let value = event.target.value;
+
+    switch (id.substring(0, id.length - 1)) {
+      case 'size':
+        setSize(value);
+        break;
+      case 'fit':
+        setFit(value);
+        break;
+      case 'length':
+        setLength(value);
+        break;
+      case 'width':
+        setWidth(value);
+        break;
+      case 'quality':
+        setQuality(value);
+        break;
+      case 'com':
+        setComfort(value);
+        break;
+      default:
+        break;
+    }
 
     setCharForm({ ...charForm, [name]: Number(event.target.value) })
   }
@@ -127,63 +154,63 @@ const NewReviewModal = ({ closeModal, metaData, productId, productName }) => {
                 type='radio'
                 value='true'
                 name='recommend'
-                checked={ recommend === value }
+                checked={ recommend === 'true' }
                 onChange={formHandler} />recommended
               <input
                 type='radio'
                 value='false'
                 name='recommend'
+                checked={ recommend === 'false' }
                 onChange={formHandler} />not recommended
             </div>
             <div className='characteristics'>
               {availableCharacteristics.includes('Size') && (<div>
                 <h4>How was the sizing?</h4>
-                <input type='radio' value='1' id='size1' name={characteristics['Size'].id} onChange={charHandler} />
-                <label for='size1'>a size too small</label>
-                <input type='radio' value='2' id='size2' name={characteristics['Size'].id} onChange={charHandler} />
-                <input type='radio' value='3' id='size3' name={characteristics['Size'].id} onChange={charHandler} />
-                <input type='radio' value='4' id='size4' name={characteristics['Size'].id} onChange={charHandler} />
-                <input type='radio' value='5' id='size5' name={characteristics['Size'].id} label='a size too big' onChange={charHandler} />
+                <input type='radio' value='1' id='size1' name={characteristics['Size'].id} onChange={charHandler} checked={sizeSelection === '1'}/>
+                <input type='radio' value='2' id='size2' name={characteristics['Size'].id} onChange={charHandler} checked={sizeSelection === '2'}/>
+                <input type='radio' value='3' id='size3' name={characteristics['Size'].id} onChange={charHandler} checked={sizeSelection === '3'}/>
+                <input type='radio' value='4' id='size4' name={characteristics['Size'].id} onChange={charHandler} checked={sizeSelection === '4'}/>
+                <input type='radio' value='5' id='size5' name={characteristics['Size'].id} label='a size too big' onChange={charHandler} checked={sizeSelection === '5'}/>
               </div>)}
               {availableCharacteristics.includes('Width') && (<div>
                 <h4>How was the width?</h4>
-                <input type='radio' value='1' id='width1' name={characteristics['Width'].id} label='too narrow' onChange={charHandler} />
-                <input type='radio' value='2' id='width2' name={characteristics['Width'].id} onChange={charHandler} />
-                <input type='radio' value='3' id='width3' name={characteristics['Width'].id} onChange={charHandler} />
-                <input type='radio' value='4' id='width4' name={characteristics['Width'].id} onChange={charHandler} />
-                <input type='radio' value='5' id='width5' name={characteristics['Width'].id} label='too wide' onChange={charHandler} />
+                <input type='radio' value='1' id='width1' name={characteristics['Width'].id} label='too narrow' onChange={charHandler} checked={widthSelection === '1'}/>
+                <input type='radio' value='2' id='width2' name={characteristics['Width'].id} onChange={charHandler} checked={widthSelection === '2'}/>
+                <input type='radio' value='3' id='width3' name={characteristics['Width'].id} onChange={charHandler} checked={widthSelection === '3'}/>
+                <input type='radio' value='4' id='width4' name={characteristics['Width'].id} onChange={charHandler} checked={widthSelection === '4'}/>
+                <input type='radio' value='5' id='width5' name={characteristics['Width'].id} label='too wide' onChange={charHandler} checked={widthSelection === '5'}/>
               </div>)}
               {availableCharacteristics.includes('Comfort') && (<div>
                 <h4>How comfortable was this?</h4>
-                <input type='radio' value='1' id='com1' name={characteristics['Comfort'].id} label='uncomfortable' onChange={charHandler} />
-                <input type='radio' value='2' id='com2' name={characteristics['Comfort'].id} onChange={charHandler} />
-                <input type='radio' value='3' id='com3' name={characteristics['Comfort'].id} onChange={charHandler} />
-                <input type='radio' value='4' id='com4' name={characteristics['Comfort'].id} onChange={charHandler} />
-                <input type='radio' value='5' id='com5' name={characteristics['Comfort'].id} label='perfect' onChange={charHandler} />
+                <input type='radio' value='1' id='com1' name={characteristics['Comfort'].id} label='uncomfortable' onChange={charHandler} checked={comfortSelection === '1'}/>
+                <input type='radio' value='2' id='com2' name={characteristics['Comfort'].id} onChange={charHandler} checked={comfortSelection === '2'}/>
+                <input type='radio' value='3' id='com3' name={characteristics['Comfort'].id} onChange={charHandler} checked={comfortSelection === '3'}/>
+                <input type='radio' value='4' id='com4' name={characteristics['Comfort'].id} onChange={charHandler} checked={comfortSelection === '4'}/>
+                <input type='radio' value='5' id='com5' name={characteristics['Comfort'].id} label='perfect' onChange={charHandler} checked={comfortSelection === '5'}/>
               </div>)}
               {availableCharacteristics.includes('Quality') && (<div>
                 <h4>How was the quality?</h4>
-                <input type='radio' value='1' id='quality1' name={characteristics['Quality'].id} label='poor' onChange={charHandler} />
-                <input type='radio' value='2' id='quality2' name={characteristics['Quality'].id} onChange={charHandler} />
-                <input type='radio' value='3' id='quality3' name={characteristics['Quality'].id} onChange={charHandler} />
-                <input type='radio' value='4' id='quality4' name={characteristics['Quality'].id} onChange={charHandler} />
-                <input type='radio' value='5' id='quality5' name={characteristics['Quality'].id} label='perfect' onChange={charHandler} />
+                <input type='radio' value='1' id='quality1' name={characteristics['Quality'].id} label='poor' onChange={charHandler} checked={qualitySelection === '1'}/>
+                <input type='radio' value='2' id='quality2' name={characteristics['Quality'].id} onChange={charHandler} checked={qualitySelection === '2'}/>
+                <input type='radio' value='3' id='quality3' name={characteristics['Quality'].id} onChange={charHandler} checked={qualitySelection === '3'}/>
+                <input type='radio' value='4' id='quality4' name={characteristics['Quality'].id} onChange={charHandler} checked={qualitySelection === '4'}/>
+                <input type='radio' value='5' id='quality5' name={characteristics['Quality'].id} label='perfect' onChange={charHandler} checked={qualitySelection === '5'}/>
               </div>)}
               {availableCharacteristics.includes('Length') && (<div>
                 <h4>How was the length?</h4>
-                <input type='radio' value='1' id='length1' name={characteristics['Length'].id} label='runs short' onChange={charHandler} />
-                <input type='radio' value='2' id='length2' name={characteristics['Length'].id} onChange={charHandler} />
-                <input type='radio' value='3' id='length3' name={characteristics['Length'].id} onChange={charHandler} />
-                <input type='radio' value='4' id='length4' name={characteristics['Length'].id} onChange={charHandler} />
-                <input type='radio' value='5' id='length5' name={characteristics['Length'].id} label='runs long' onChange={charHandler} />
+                <input type='radio' value='1' id='length1' name={characteristics['Length'].id} label='runs short' onChange={charHandler} checked={lengthSelection === '1'}/>
+                <input type='radio' value='2' id='length2' name={characteristics['Length'].id} onChange={charHandler} checked={lengthSelection === '2'}/>
+                <input type='radio' value='3' id='length3' name={characteristics['Length'].id} onChange={charHandler} checked={lengthSelection === '3'}/>
+                <input type='radio' value='4' id='length4' name={characteristics['Length'].id} onChange={charHandler} checked={lengthSelection === '4'}/>
+                <input type='radio' value='5' id='length5' name={characteristics['Length'].id} label='runs long' onChange={charHandler} checked={lengthSelection === '5'}/>
               </div>)}
               {availableCharacteristics.includes('Fit') && (<div>
                 <h4>How was the fit?</h4>
-                <input type='radio' value='1' id='fit1' name={characteristics['Fit'].id} label='runs short' onChange={charHandler} />
-                <input type='radio' value='2' id='fit2' name={characteristics['Fit'].id} onChange={charHandler} />
-                <input type='radio' value='3' id='fit3' name={characteristics['Fit'].id} onChange={charHandler} />
-                <input type='radio' value='4' id='fit1' name={characteristics['Fit'].id} onChange={charHandler} />
-                <input type='radio' value='5' id='fit5' name={characteristics['Fit'].id} label='runs long' onChange={charHandler} />
+                <input type='radio' value='1' id='fit1' name={characteristics['Fit'].id} label='runs short' onChange={charHandler} checked={fitSelection === '1'}/>
+                <input type='radio' value='2' id='fit2' name={characteristics['Fit'].id} onChange={charHandler} checked={fitSelection === '2'}/>
+                <input type='radio' value='3' id='fit3' name={characteristics['Fit'].id} onChange={charHandler} checked={fitSelection === '3'}/>
+                <input type='radio' value='4' id='fit1' name={characteristics['Fit'].id} onChange={charHandler} checked={fitSelection === '4'}/>
+                <input type='radio' value='5' id='fit5' name={characteristics['Fit'].id} label='runs long' onChange={charHandler} checked={fitSelection === '5'}/>
               </div>)}
             </div>
             <h4>Review Summary</h4>
